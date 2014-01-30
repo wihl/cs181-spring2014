ea86ac1179d4f35fa4310f0df7509bda41c7f04d
David Wihl <wihl@ripariandata.com>
2014-01-30 14:57:09 -0500

    merging in Ng ex7 for Kmeans

--- Practical1/warmup/first.m
+++ Practical1/warmup/first.m
@@ -1,4 +1,6 @@
 
+clear; close all; clc
+
 K = 5;
 setSize = 10;
 X = [];
@@ -9,6 +11,16 @@ for i = 0:(K-1)
   i = i + 1;
 end
 
+%initial_centroids = [ 5 5; 15 15; 28 28; 38 38; 42 42];
+initial_centroids = kMeansInitCentroids(X, K);
 
+idx = findClosestCentroids(X, initial_centroids);
 
+centroids = computeCentroids(X, idx, K);
 
+max_iters = 10;
+
+[centroids, idx] = runkMeans(X, initial_centroids, max_iters, true);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
