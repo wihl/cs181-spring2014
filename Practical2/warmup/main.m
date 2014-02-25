577ba90cb1d92d5bc1b8f2c6a3a860ed8681f6af
David Wihl <wihl@ripariandata.com>
2014-02-24 20:12:25 -0500

    cleanup of Ridge complete

--- Practical2/warmup/main.m
+++ Practical2/warmup/main.m
@@ -20,7 +20,7 @@ m = length(Y); % number of training examples
 fprintf('Press enter to calculate and plot gradient descent.\n');
 pause;
 
-% compute via gradient descent
+% compute via linear fit via gradient descent
 X_bias = [ones(m, 1), input(:,1)]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
 
@@ -31,7 +31,7 @@ alpha = 0.001;
 % run gradient descent
 theta = gradientDescent(X_bias, Y, theta, alpha, iterations);
 
-fprintf('Final gradient descent cost is %f\n', computeCost(X_bias, Y, X_bias*theta));
+fprintf('Final linear basis cost is %f\n', computeCost(X_bias, Y, X_bias*theta));
 hold on;
 plotRegression(X,Y,X_bias*theta,'Linear Basis')
 
@@ -61,7 +61,7 @@ fprintf('Final polynomial cost is %f\n', bestJ);
 plotRegression(X,Y,f,'Polynomial Basis')
 
 %
-% Part 3 - closed form
+% Part 3 - Gaussian with Moore Penrose
 %
 fprintf('Press enter to use Gaussian.\n');
 pause;
@@ -70,54 +70,7 @@ pause;
 theta = pinv(X_bias'*X_bias)*X_bias'*Y;
 
 J = computeCost(X,Y,X_bias*theta);
-fprintf('Cost via Gaussian%f\n',J);
+fprintf('Cost via Gaussian %f\n',J);
 
 plotRegression(X,Y,X_bias*theta,'Gaussian / Moore Penrose');
 
-%
-% Part 4 - 
-% Modified from "Machine Learning, a Probablistic Perspective"
-% by Kevin Murphy. Original code can be found at 
-% https://github.com/probml/pmtk3
-%
-fprintf('Press enter to use Gaussian noise.\n');
-pause;
-
-trainingPoints = length(Y);
-
-a0 = -0.3; %Parameters of the actual underlying model that we wish to recover
-a1 = 0.5;  %We will estimate these values with w0 and w1 respectively. 
-
-noiseSD = 0.2;
-
-priorPrecision = 2.0;
-
-likelihoodSD = noiseSD;
-likelihoodPrecision = 1/(likelihoodSD)^2;
-
-xtrain = X;
-ytrain = Y;
-
-model = struct('mu', 0, 'Sigma', noiseSD);
-noise = gaussSample(model, trainingPoints);
-
-iter = 30;
-
-priorMean = [0;0];
-priorSigma = eye(2)./priorPrecision; %Covariance Matrix
-priorPDF = @(W)gaussProb(W,priorMean',priorSigma);
-
-mu = priorMean;
-sigma = priorSigma;
-
-for i=1:iter
-  likelihood = @(W) uniGaussPdf(xtrain(i),W*[1;xtrain(i)],likelihoodSD.^2);
-
-  [postW,mu,sigma] = update([1,xtrain(i)],ytrain(i),likelihoodPrecision,mu,sigma);
-
-end
-
-%J = computeCost(X,Y,X_bias*theta);
-%fprintf('Cost via closed form %f\n',J);
-
-%plotRegression(X,Y,X_bias*theta,'Closed form');
