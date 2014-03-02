b02023d4e67a449b97f0948a4461ade9b9230409
David Wihl <wihl@ripariandata.com>
2014-03-02 10:44:02 -0500

    refactored and cleaned up vectorized split

---
+++ Practical3/solution/featurefunc.py
@@ -0,0 +1,165 @@
+import os
+from collections import Counter
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+import numpy as np
+from scipy import sparse
+
+import util
+
+
+def extract_feats(ffs, direc="train", global_feat_dict=None):
+    """
+    arguments:
+      ffs are a list of feature-functions.
+      direc is a directory containing xml files (expected to be train or test).
+      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
+      should only be provided when extracting features from test data, so that 
+      the columns of the test matrix align correctly.
+
+    returns: 
+      a sparse design matrix, a dict mapping features to column-numbers,
+      a vector of target classes, and a list of system-call-history ids in order 
+      of their rows in the design matrix.
+      
+      Note: the vector of target classes returned will contain the true indices of the
+      target classes on the training data, but will contain only -1's on the test
+      data
+    """
+    fds = [] # list of feature dicts
+    classes = []
+    ids = [] 
+    for datafile in os.listdir(direc):
+        # extract id and true class (if available) from filename
+        id_str,clazz = datafile.split('.')[:2]
+        ids.append(id_str)
+        # add target class if this is training data
+        try:
+            classes.append(util.malware_classes.index(clazz))
+        except ValueError:
+            # we should only fail to find the label in our list of malware classes
+            # if this is test data, which always has an "X" label
+            assert clazz == "X"
+            classes.append(-1)
+        rowfd = {}
+        # parse file as an xml document
+        tree = ET.parse(os.path.join(direc,datafile))
+        # accumulate features
+        [rowfd.update(ff(tree)) for ff in ffs]
+        fds.append(rowfd)
+        
+
+    X,feat_dict = make_design_mat(fds,global_feat_dict)
+
+    return X, feat_dict, np.array(classes), ids
+
+
+def make_design_mat(fds, global_feat_dict=None):
+    """
+    arguments:
+      fds is a list of feature dicts (one for each row).
+      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
+      should only be provided when extracting features from test data, so that 
+      the columns of the test matrix align correctly.
+       
+    returns: 
+        a sparse NxD design matrix, where N == len(fds) and D is the number of
+        the union of features defined in any of the fds 
+    """
+    if global_feat_dict is None:
+        all_feats = set()
+        [all_feats.update(fd.keys()) for fd in fds]
+        feat_dict = dict([(feat, i) for i, feat in enumerate(sorted(all_feats))])
+    else:
+        feat_dict = global_feat_dict
+        
+    cols = []
+    rows = []
+    data = []        
+    for i in xrange(len(fds)):
+        temp_cols = []
+        temp_data = []
+        for feat,val in fds[i].iteritems():
+            try:
+                # update temp_cols iff update temp_data
+                temp_cols.append(feat_dict[feat])
+                temp_data.append(val)
+            except KeyError as ex:
+                if global_feat_dict is not None:
+                    pass  # new feature in test data; nbd
+                else:
+                    raise ex
+
+        # all fd's features in the same row
+        k = len(temp_cols)
+        cols.extend(temp_cols)
+        data.extend(temp_data)
+        rows.extend([i]*k)
+
+    assert len(cols) == len(rows) and len(rows) == len(data)
+   
+
+    X = sparse.csr_matrix((np.array(data),
+                   (np.array(rows), np.array(cols))),
+                   shape=(len(fds), len(feat_dict)))
+    return X, feat_dict    
+
+
+## Here are two example feature-functions. They each take an xml.etree.ElementTree object, 
+# (i.e., the result of parsing an xml file) and returns a dictionary mapping 
+# feature-names to numeric values.
+## TODO: modify these functions, and/or add new ones.
+def first_last_system_call_feats(tree):
+    """
+    arguments:
+      tree is an xml.etree.ElementTree object
+    returns:
+      a dictionary mapping 'first_call-x' to 1 if x was the first system call
+      made, and 'last_call-y' to 1 if y was the last system call made. 
+      (in other words, it returns a dictionary indicating what the first and 
+      last system calls made by an executable were.)
+    """
+    c = Counter()
+    in_all_section = False
+    first = True # is this the first system call
+    last_call = None # keep track of last call we've seen
+    for el in tree.iter():
+        # ignore everything outside the "all_section" element
+        if el.tag == "all_section" and not in_all_section:
+            in_all_section = True
+        elif el.tag == "all_section" and in_all_section:
+            in_all_section = False
+        elif in_all_section:
+            if first:
+                c["first_call-"+el.tag] = 1
+                first = False
+            last_call = el.tag  # update last call seen
+            
+    # finally, mark last call seen
+    c["last_call-"+last_call] = 1
+    return c
+
+def system_call_count_feats(tree):
+    """
+    arguments:
+      tree is an xml.etree.ElementTree object
+    returns:
+      a dictionary mapping 'num_system_calls' to the number of system_calls
+      made by an executable (summed over all processes)
+    """
+    c = Counter()
+    in_all_section = False
+    for el in tree.iter():
+        # ignore everything outside the "all_section" element
+        if el.tag == "all_section" and not in_all_section:
+            in_all_section = True
+        elif el.tag == "all_section" and in_all_section:
+            in_all_section = False
+        elif in_all_section:
+            c['num_system_calls'] += 1
+    return c
+
+def getFeatures():
+    return [first_last_system_call_feats, system_call_count_feats]
--- Practical3/solution/vectorize.py
+++ Practical3/solution/vectorize.py
@@ -3,10 +3,6 @@
   Take one or more source XML files, extract features and write to a YAML file
 '''
 
-
-
-
-import os
 from collections import Counter
 try:
     import xml.etree.cElementTree as ET
@@ -16,177 +12,9 @@ import numpy as np
 from scipy import sparse
 import yaml
 
-
+import featurefunc
 import util
 
-
-def extract_feats(ffs, direc="train", global_feat_dict=None):
-    """
-    arguments:
-      ffs are a list of feature-functions.
-      direc is a directory containing xml files (expected to be train or test).
-      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
-      should only be provided when extracting features from test data, so that 
-      the columns of the test matrix align correctly.
-
-    returns: 
-      a sparse design matrix, a dict mapping features to column-numbers,
-      a vector of target classes, and a list of system-call-history ids in order 
-      of their rows in the design matrix.
-      
-      Note: the vector of target classes returned will contain the true indices of the
-      target classes on the training data, but will contain only -1's on the test
-      data
-    """
-    fds = [] # list of feature dicts
-    classes = []
-    ids = [] 
-    for datafile in os.listdir(direc):
-        # extract id and true class (if available) from filename
-        id_str,clazz = datafile.split('.')[:2]
-        ids.append(id_str)
-        # add target class if this is training data
-        try:
-            classes.append(util.malware_classes.index(clazz))
-        except ValueError:
-            # we should only fail to find the label in our list of malware classes
-            # if this is test data, which always has an "X" label
-            assert clazz == "X"
-            classes.append(-1)
-        rowfd = {}
-        # parse file as an xml document
-        tree = ET.parse(os.path.join(direc,datafile))
-        # accumulate features
-        [rowfd.update(ff(tree)) for ff in ffs]
-        fds.append(rowfd)
-        
-
-    X,feat_dict = make_design_mat(fds,global_feat_dict)
-
-
-    # write out vectorized result
-    # see http://stackoverflow.com/questions/8955448/save-load-scipy-sparse-csr-matrix-in-portable-data-format
-    with open('data.npy', 'w') as outfile:
-        np.save(outfile, X)
-
-    with open('features.npy','w') as outfile:
-        np.save(outfile, np.array(classes))
-
-    with open('features.yaml', 'w') as outfile:
-        outfile.write( yaml.dump(feat_dict, default_flow_style=True) )
-
-    with open('ids.yaml', 'w') as outfile:
-        outfile.write( yaml.dump(ids, default_flow_style=True) )
-
-
-
-    return X, feat_dict, np.array(classes), ids
-
-
-def make_design_mat(fds, global_feat_dict=None):
-    """
-    arguments:
-      fds is a list of feature dicts (one for each row).
-      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
-      should only be provided when extracting features from test data, so that 
-      the columns of the test matrix align correctly.
-       
-    returns: 
-        a sparse NxD design matrix, where N == len(fds) and D is the number of
-        the union of features defined in any of the fds 
-    """
-    if global_feat_dict is None:
-        all_feats = set()
-        [all_feats.update(fd.keys()) for fd in fds]
-        feat_dict = dict([(feat, i) for i, feat in enumerate(sorted(all_feats))])
-    else:
-        feat_dict = global_feat_dict
-        
-    cols = []
-    rows = []
-    data = []        
-    for i in xrange(len(fds)):
-        temp_cols = []
-        temp_data = []
-        for feat,val in fds[i].iteritems():
-            try:
-                # update temp_cols iff update temp_data
-                temp_cols.append(feat_dict[feat])
-                temp_data.append(val)
-            except KeyError as ex:
-                if global_feat_dict is not None:
-                    pass  # new feature in test data; nbd
-                else:
-                    raise ex
-
-        # all fd's features in the same row
-        k = len(temp_cols)
-        cols.extend(temp_cols)
-        data.extend(temp_data)
-        rows.extend([i]*k)
-
-    assert len(cols) == len(rows) and len(rows) == len(data)
-   
-
-    X = sparse.csr_matrix((np.array(data),
-                   (np.array(rows), np.array(cols))),
-                   shape=(len(fds), len(feat_dict)))
-    return X, feat_dict
-    
-
-## Here are two example feature-functions. They each take an xml.etree.ElementTree object, 
-# (i.e., the result of parsing an xml file) and returns a dictionary mapping 
-# feature-names to numeric values.
-## TODO: modify these functions, and/or add new ones.
-def first_last_system_call_feats(tree):
-    """
-    arguments:
-      tree is an xml.etree.ElementTree object
-    returns:
-      a dictionary mapping 'first_call-x' to 1 if x was the first system call
-      made, and 'last_call-y' to 1 if y was the last system call made. 
-      (in other words, it returns a dictionary indicating what the first and 
-      last system calls made by an executable were.)
-    """
-    c = Counter()
-    in_all_section = False
-    first = True # is this the first system call
-    last_call = None # keep track of last call we've seen
-    for el in tree.iter():
-        # ignore everything outside the "all_section" element
-        if el.tag == "all_section" and not in_all_section:
-            in_all_section = True
-        elif el.tag == "all_section" and in_all_section:
-            in_all_section = False
-        elif in_all_section:
-            if first:
-                c["first_call-"+el.tag] = 1
-                first = False
-            last_call = el.tag  # update last call seen
-            
-    # finally, mark last call seen
-    c["last_call-"+last_call] = 1
-    return c
-
-def system_call_count_feats(tree):
-    """
-    arguments:
-      tree is an xml.etree.ElementTree object
-    returns:
-      a dictionary mapping 'num_system_calls' to the number of system_calls
-      made by an executable (summed over all processes)
-    """
-    c = Counter()
-    in_all_section = False
-    for el in tree.iter():
-        # ignore everything outside the "all_section" element
-        if el.tag == "all_section" and not in_all_section:
-            in_all_section = True
-        elif el.tag == "all_section" and in_all_section:
-            in_all_section = False
-        elif in_all_section:
-            c['num_system_calls'] += 1
-    return c
 
 ## The following function does the feature extraction, learning, and prediction
 def main():
@@ -195,13 +23,38 @@ def main():
     outputfile = "mypredictions.csv"  # feel free to change this or take it as an argument
     
     # TODO put the names of the feature functions you've defined above in this list
-    ffs = [first_last_system_call_feats, system_call_count_feats]
+    ffs = featurefunc.getFeatures()
     
     # extract features
     print "extracting training features..."
-    X_train,global_feat_dict,t_train,train_ids = extract_feats(ffs, train_dir)
+    X_train,global_feat_dict,t_train,train_ids = featurefunc.extract_feats(ffs, train_dir)
     print "done extracting training features"
     print
+    
+    # TODO train here, and learn your classification parameters
+    print "learning..."
+    learned_W = np.random.random((len(global_feat_dict),len(util.malware_classes)))
+    print "done learning"
+    print
+
+    # write out vectorized result
+    # see http://stackoverflow.com/questions/8955448/save-load-scipy-sparse-csr-matrix-in-portable-data-format
+    print "serializing training features..."
+    with open('data.npy', 'w') as outfile:
+        np.save(outfile, X_train)
+
+    with open('features.npy','w') as outfile:
+        np.save(outfile, t_train)
+
+    with open('learned_w.npy','w') as outfile:
+        np.save(outfile, learned_W)
+
+    with open('features.yaml', 'w') as outfile:
+        outfile.write( yaml.dump(global_feat_dict, default_flow_style=True) )
+
+    with open('ids.yaml', 'w') as outfile:
+        outfile.write( yaml.dump(train_ids, default_flow_style=True) )
+    print "done"
 
 
 
