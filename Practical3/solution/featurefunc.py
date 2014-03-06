0aa09a931277361447524e997f2dbb33183e7c9e
David Wihl <wihl@ripariandata.com>
2014-03-06 11:38:22 -0500

    features refactored - thank goodness

--- Practical3/solution/featurefunc.py
+++ Practical3/solution/featurefunc.py
@@ -9,150 +9,88 @@ from scipy import sparse
 
 import util
 
-def extract_feats_orig(ffs, direc="train", global_feat_dict=None):
-    """
-    arguments:
-      ffs are a list of feature-functions.
-      direc is a directory containing xml files (expected to be train or test).
-      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
-      should only be provided when extracting features from test data, so that 
-      the columns of the test matrix align correctly.
+class Dataset(object):
+    '''
+    Create and manage datasets
+    '''
+    def __init__(self):
+        self.fds = []
+        self.featureDict = None
+        self.ffs = self.getFeatures()
 
-    returns: 
-      a sparse design matrix, a dict mapping features to column-numbers,
-      a vector of target classes, and a list of system-call-history ids in order 
-      of their rows in the design matrix.
-      
-      Note: the vector of target classes returned will contain the true indices of the
-      target classes on the training data, but will contain only -1's on the test
-      data
-    """
-    fds = [] # list of feature dicts
-    classes = []
-    ids = [] 
-    for datafile in os.listdir(direc):
-        # extract id and true class (if available) from filename
-        id_str,clazz = datafile.split('.')[:2]
-        ids.append(id_str)
-        # add target class if this is training data
-        try:
-            classes.append(util.malware_classes.index(clazz))
-        except ValueError:
-            # we should only fail to find the label in our list of malware classes
-            # if this is test data, which always has an "X" label
-            assert clazz == "X"
-            classes.append(-1)
+    def getFeatures(self):
+        return [first_last_system_call_feats, system_call_count_feats, process_metrics]
+
+    def getDataset(self,directory):
+        self.ids = []
+        self.directory = directory
+        y = []
+        for datafile in os.listdir(self.directory):
+            # extract id and true class (if available) from filename
+            id_str,clazz = datafile.split('.')[:2]
+            self.ids.append(id_str)
+            # add target class if this is training data
+            if clazz != "X":
+                y.append(util.malware_classes.index(clazz))
+
+            self.extractFeaturesByFile(datafile)
+
+        X = self.makeDesignMat()
+
+        return X, y, self.ids
+
+
+    def extractFeaturesByFile(self, datafile):
         rowfd = {}
         # parse file as an xml document
-        tree = ET.parse(os.path.join(direc,datafile))
+        tree = ET.parse(os.path.join(self.directory,datafile))
         # accumulate features
-        [rowfd.update(ff(tree)) for ff in ffs]
-        fds.append(rowfd)
-        
-
-    X,feat_dict = make_design_mat(fds,global_feat_dict)
-
-    return X, feat_dict, np.array(classes), ids
+        [rowfd.update(ff(tree)) for ff in self.ffs]
+        self.fds.append(rowfd)
+        return
 
 
+    def makeDesignMat(self):
+        if self.featureDict is None:
+            all_feats = set()
+            [all_feats.update(fd.keys()) for fd in self.fds]
+            feat_dict = dict([(feat, i) for i, feat in enumerate(sorted(all_feats))])
+        else:
+            feat_dict = self.featureDict
+            
+        cols = []
+        rows = []
+        data = []        
+        for i in xrange(len(self.fds)):
+            temp_cols = []
+            temp_data = []
+            for feat,val in self.fds[i].iteritems():
+                try:
+                    # update temp_cols iff update temp_data
+                    temp_cols.append(feat_dict[feat])
+                    temp_data.append(val)
+                except KeyError as ex:
+                    if self.featureDict is not None:
+                        pass  # new feature in test data; nbd
+                    else:
+                        raise ex
+
+            # all fd's features in the same row
+            k = len(temp_cols)
+            cols.extend(temp_cols)
+            data.extend(temp_data)
+            rows.extend([i]*k)
+
+        assert len(cols) == len(rows) and len(rows) == len(data)
+    
+
+        self.featureDict = feat_dict
+        X = sparse.csr_matrix((np.array(data),
+                           (np.array(rows), np.array(cols))),
+                              shape=(len(self.fds), len(self.featureDict)))
+        return X
 
 
-
-class ExtractorFds(object):
-    fds = []  # class (static) variable
-
-    def append(row):
-        fds.append(row)
-
-    def getFds():
-        return fds
-
-def extract_feats_by_dir(direc):
-    fds = ExtractorFds()
-    ffs = getFeatures()
-    classes = {}
-    ids = []
-    for datafile in os.listdir(direc):
-        # extract id and true class (if available) from filename
-        id_str,clazz = datafile.split('.')[:2]
-        ids.append(id_str)
-        # add target class if this is training data
-        if clazz != "X":
-            classes[id_str] = util.malware_classes.index(clazz)
-
-        extract_feats_by_file(ffs, fds, direc, datafile)
-    X,feat_dict = make_design_mat(fds,None)
-    y = [classes[item] for item in ids]
-
-    return X, y, ids
-
-
-def extract_feats_by_file(ffs, direc, datafile):
-    fds = ExtractorFds()
-    rowfd = {}
-    # parse file as an xml document
-    tree = ET.parse(os.path.join(direc,datafile))
-    # accumulate features
-    [rowfd.update(ff(tree)) for ff in ffs]
-    fds.append(rowfd)
-    return
-
-
-def make_design_mat(fds, global_feat_dict=None):
-    """
-    arguments:
-      fds is a list of feature dicts (one for each row).
-      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
-      should only be provided when extracting features from test data, so that 
-      the columns of the test matrix align correctly.
-       
-    returns: 
-        a sparse NxD design matrix, where N == len(fds) and D is the number of
-        the union of features defined in any of the fds 
-    """
-    if global_feat_dict is None:
-        all_feats = set()
-        [all_feats.update(fd.keys()) for fd in fds]
-        feat_dict = dict([(feat, i) for i, feat in enumerate(sorted(all_feats))])
-    else:
-        feat_dict = global_feat_dict
-        
-    cols = []
-    rows = []
-    data = []        
-    for i in xrange(len(fds)):
-        temp_cols = []
-        temp_data = []
-        for feat,val in fds[i].iteritems():
-            try:
-                # update temp_cols iff update temp_data
-                temp_cols.append(feat_dict[feat])
-                temp_data.append(val)
-            except KeyError as ex:
-                if global_feat_dict is not None:
-                    pass  # new feature in test data; nbd
-                else:
-                    raise ex
-
-        # all fd's features in the same row
-        k = len(temp_cols)
-        cols.extend(temp_cols)
-        data.extend(temp_data)
-        rows.extend([i]*k)
-
-    assert len(cols) == len(rows) and len(rows) == len(data)
-   
-
-    X = sparse.csr_matrix((np.array(data),
-                   (np.array(rows), np.array(cols))),
-                   shape=(len(fds), len(feat_dict)))
-    return X, feat_dict    
-
-
-## Here are two example feature-functions. They each take an xml.etree.ElementTree object, 
-# (i.e., the result of parsing an xml file) and returns a dictionary mapping 
-# feature-names to numeric values.
-## TODO: modify these functions, and/or add new ones.
 def first_last_system_call_feats(tree):
     """
     arguments:
@@ -213,7 +151,4 @@ def process_metrics(tree):
                 if el.get(r) != None:
                     c[r+'-'+el.attrib[r]] = c.get(r+'-'+el.attrib[r],0) + 1
     return c
-
-def getFeatures():
-    return [first_last_system_call_feats, system_call_count_feats, process_metrics]
 
