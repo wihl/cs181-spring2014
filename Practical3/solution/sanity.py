9c82f63590b47f58864c51d1a1b9320acf42d65d
David Wihl <wihl@ripariandata.com>
2014-03-10 09:17:28 -0400

    updated notes; added plotting to sanity check

--- Practical3/solution/sanity.py
+++ Practical3/solution/sanity.py
@@ -4,6 +4,12 @@ sanity check of prediction file before submission
 
 import argparse
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+
+origDistributions = [3.69, 1.62, 1.2, 1.03, 1.33, 1.26, 1.72, 1.33, 52.14, 0.68, 17.56, 1.04, 12.18, 1.91, 1.30]
+virusNames = ['Agent','AutoRun','FraudLoad','FraudPack','Hupigon','Krap','Lipler','Magania','None','Poison','Swizzor','Tdss','VB','Virut','Zbot']
 
 def main():
     parser = argparse.ArgumentParser()
@@ -25,9 +31,22 @@ def main():
             assert pred < 15
             counts[pred] += 1
 
+    newDistributions = [0] * len(origDistributions)
     print "total predictions", sum(counts), "rows",rows
     for i, val in enumerate(counts):
-        print i,'\t',float(val)/ float(rows) * 100.0
+        newDistributions[i] = float(val)/ float(rows) * 100.0
+
+    width = 0.35
+    ind = np.arange(len(origDistributions))
+    fig, ax = pl.subplots()
+    rects1 = ax.bar(ind, origDistributions, width, color = 'b')
+    rects2 = ax.bar(ind+width, newDistributions, width, color = 'r')
+    ax.legend( (rects1[0], rects2[0]), ('Original','Predicted') )
+    ax.set_xticks(ind + width/2)
+    ax.set_xticklabels(virusNames, rotation=90)
+    ax.set_title('Prior Day vs Predicted Distribution')
+    ax.set_ylabel('Percent')
+    pl.show()
 
 if __name__ == "__main__":
     main()
