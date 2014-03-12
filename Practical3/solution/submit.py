6a64c590beabefd9b2e920957de0a734fc1a81f4
David Wihl <wihl@ripariandata.com>
2014-03-12 17:25:26 -0400

    fleshed out some of writeup

--- Practical3/solution/submit.py
+++ Practical3/solution/submit.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from scipy import sparse
+import itertools as iter
 
 import featurefunc as ff
 import classifiers as cl
@@ -16,26 +17,38 @@ def main():
     lr = cl.LogisticRegression()
     knn = cl.kNN()
     
-    ds = ff.Dataset()
+    ds = ff.Dataset(ff.MetricType().thread)
 
     print "training..."
 
     X, y, ids = ds.getDataset(train_dir)
     lr.fit(X,y)
-    knn.fit(X,y)
+    #knn.fit(X,y)
     print "training complete. Now preparing for submit"
     # TODO - add in thread classification
 
     X, y, ids = ds.getDataset(test_dir)
+    print "threadmapping:", ds.threadMapping
+    syscallMap = sorted(ds.threadMapping)
+    mapPos = 0
     
     predsLR = lr.predict(X)
-    pbLR    = lr.classifier_().predict_proba(X)
+    #pbLR    = lr.classifier_().predict_proba(X)
 
-    predskNN = knn.predict(X)
-    pbkNN = knn.classifier_().predict_proba(X)
+    #predskNN = knn.predict(X)
+    #pbkNN = knn.classifier_().predict_proba(X)
 
     finalpred = []
     for i in xrange(len(predsLR)):
+        # find the best syscall prediction for this id
+        print i
+        for j in syscallMap:
+            print j
+            break
+        beg = next(iter.islice(syscallMap,i,None))
+        end = next(iter.islice(syscallMap,i+1,None))
+        print syscallMap, beg, end, predsLR[beg:end]
+        '''
         if np.max(pbkNN[i]) > 0.8:
             # if kNN is more .8 sure, it is very accurate
             choice = predskNN[i]
@@ -45,9 +58,10 @@ def main():
         else:
             choice = predsLR[i]
         finalpred.append(choice)
+        '''
     
     print "writing predictions..."
-    util.write_predictions(finalpred, ids, outputfile)
+    #util.write_predictions(finalpred, ids, outputfile)
     print "done!"
 
 if __name__ == "__main__":
