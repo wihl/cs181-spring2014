620043661ac9c86cc4a1cc29ac0df6ae01dd29e3
David Wihl <wihl@ripariandata.com>
2014-04-17 06:04:22 -0400

    basic game working with preset value matrix

--- Practical4/warmup/rl.py
+++ Practical4/warmup/rl.py
@@ -3,12 +3,10 @@ import random
 
 class Learner(object):
     def __init__(self,rows,cols,grid,targetScore,maxscore):
-        self.reward = 0
         self.states = range(maxscore + 1)
         self.rows = rows
         self.cols = cols
         self.currentState = 0
-        self.reward = 0
         self.targetScore = targetScore
         self.grid = grid
         self.maxthrow = max(max(grid))
@@ -29,42 +27,29 @@ class Learner(object):
             else:
             # we're over - fill in the rest with zeroes
                 valueMatrix.append((0,0))
-        print valueMatrix
         return valueMatrix
             
 
     def findLocation(self,desiredScore):
-        print "desired",desiredScore
         for x in range(1,self.rows+1):
             for y in range(1,self.cols+1):
                 if self.grid[x][y] == desiredScore:
-                    print "trying ",x,y
                     return x, y
-        print "did not find it"
-        return 1,1
+        return 1,1 # defensive
 
     def getPolicy(self):
         # based on the current state, find the best throw
-        desiredScore = self.targetScore - self.currentState
-        x = random.randint(1,self.rows)
-        y = random.randint(1,self.cols)
-        if desiredScore < self.maxthrow:
-            # we're close - aim for the closest
-            print "getting close..."
-            x, y = self.findLocation(desiredScore)
+        x = self.valueMatrix[self.currentState][0]
+        y = self.valueMatrix[self.currentState][1]
         return x, y
 
     def action(self, grid):
         self.grid = grid
         x, y = self.getPolicy()
-        print x, y
         return x, y
 
     def setState(self,newState):
         self.currentState = newState
-
-    def setReward(self,newReward):
-        self.reward = newReward
 
 def playgame():
     grid = g.Grid()
@@ -79,10 +64,10 @@ def playgame():
         grid.printGrid()
 
         x, y = learner.action(grid.getGrid())
+        print "aiming for", x, y
         round = grid.noisyThrow(x, y)
         score += round
         learner.setState(score)
-        learner.setReward(grid.reward(score))
         print "You got ",round," Current score:", score
     if grid.reward(score) == -1:
         print "You lose."
